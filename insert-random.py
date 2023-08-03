ac5c7e396303259959a51d033616369b18e0521d
milan9527 <milan9527@hotmail.com>
2023-08-03 11:11:31 +0800

    Update insert-random.py

--- insert-random.py
+++ insert-random.py
@@ -12,7 +12,7 @@ def generate_random_string(size):
 
 random_string = generate_random_string(4 * 1024)
 idn = 0
-client = pymongo.MongoClient('mongodb://milan:wAr16dk7@ping5.cluster-c7b8fns5un9o.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false')
+client = pymongo.MongoClient('mongodb://milan:password@ping5.cluster-c7b8fns5un9o.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false')
 db = client.test
 col = db.micro
 for num in range(0, 100000):
